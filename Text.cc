790810d1b55b320eca91a8b2cb9e59b72c586293
marbadia97 <marbadia97@gmail.com>
2016-05-11 16:29:29 +0200

    text.cc

---
+++ Text.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <map>
+#include <list>
+
+class Text {
+	string Titol;
+	vector<string> frases;
+	map <string,int> frequencies;
+};
