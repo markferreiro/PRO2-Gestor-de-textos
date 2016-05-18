e56aa87b282d74a70e0bce76d524cecf69332053
Marc Ferreiro <ferrertecno@gmail.com>
2016-05-18 17:30:08 +0200

    Estructura

---
+++ Conjunt_autors.cc
@@ -0,0 +1,30 @@
+#include "Autor.hh"
+#include <vector>
+#include <string>
+#include <set>
+
+class Conjunt_autors {
+	private set<Autor> Autors;
+	
+	Conjunt_autors() {
+		
+	}
+	bool afegir_autor(string nom) {
+		
+	}
+	void eliminar_autor(string nom) {
+		
+	}
+	bool existeix_titol(string titol) {
+		
+	}
+	Autor obtenir_autor(string nom) {
+		
+	}
+	set<Autor> tots_autors() {
+		
+	}
+	bool triar_text (vector<string> paraules) {
+		
+	}
+};
